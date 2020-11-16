c5367780bee979b6c653e18e786a2753f0b0727b
Marcin Wierzbiński <marcin119a@gmail.com>
2020-11-16 15:43:14 +0100

    init work

---
+++ model.py
@@ -0,0 +1,62 @@
+from sklearn.preprocessing import MinMaxScaler
+import pandas
+import matplotlib.pyplot as plt
+from utilities import create_dataset
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+import math
+
+def model(file_name, number_neurons, epochs=100, batch_size=1, loss='mean_squared_error', ): 
+
+    dataset = pandas.read_csv(file_name, usecols=[1], engine='python')
+    plt.plot(dataset)
+    plt.show()
+
+    # normalize the dataset
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    dataset = scaler.fit_transform(dataset)
+
+    train_size = int(len(dataset) * 0.67)
+    test_size = len(dataset) - train_size
+    train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+    print(len(train), len(test))
+
+    look_back = 1
+    trainX, trainY = create_dataset(train, look_back)
+    testX, testY = create_dataset(test, look_back)
+
+    # reshape input to be [samples, time steps, features]
+    trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+    testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+    
+    model = Sequential()
+    model.add(LSTM(number_neurons, input_shape=(look_back, 1)))
+    model.add(Dense(1))
+    model.add(LSTM(number_neurons))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(trainX, trainY, epochs=epochs, batch_size=batch_size, verbose=2)
+    # make predictions
+    trainPredict = model.predict(trainX)
+    testPredict = model.predict(testX)
+    # invert predictions
+    trainPredict = scaler.inverse_transform(trainPredict)
+    trainY = scaler.inverse_transform([trainY])
+    testPredict = scaler.inverse_transform(testPredict)
+    testY = scaler.inverse_transform([testY])
+    # calculate root mean squared error
+    trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+    print('Train Score: %.2f RMSE' % (trainScore))
+    testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+    print('Test Score: %.2f RMSE' % (testScore))
+    # shift train predictions for plotting
+    trainPredictPlot = numpy.empty_like(dataset)
+    trainPredictPlot[:, :] = numpy.nan
+    trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+    # shift test predictions for plotting
+    testPredictPlot = numpy.empty_like(dataset)
+    testPredictPlot[:, :] = numpy.nan
+    testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+    # plot baseline and predictions
+
